d849a00c8d234e6bb71b6d6fc198047d96b211d8
leonardo.giglio <giglioleonardo@hotmail.com>
2022-08-16 09:51:00 -0300

    catalogo_libros

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+
+//CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Setup Variables
+const books = [
+{
+  img: 'https://m.media-amazon.com/images/I/91xUsdujK3L._AC_UL640_QL65_.jpg',
+  title: 'Cat Kid Comic Club',
+  autor: 'Dav Pilkey',
+},
+{
+  img: 'https://m.media-amazon.com/images/I/916OF0f3xvL._AC_UL640_QL65_.jpg',
+  title: 'The Year We Learned to Fly',
+  autor: 'Jacqueline Woodson y Rafael López',
+},
+{
+  img: 'https://m.media-amazon.com/images/I/819CF9e7CPL._AC_UL640_QL65_.jpg',
+  title: 'The Marvellers',
+  autor: 'Dhonielle Clayton',
+},
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+      return (
+      <Book book={book}></Book>
+      );
+    })}
+    </section>
+  );
+}
+
+const Book = (props) => {
+  const {img, title, autor} = props.book;
+  return (
+  <article className='book'>
+    <img src={img} alt=''/>
+    <h1>{title}</h1>
+    <h4 >{autor}</h4>
+  </article>
+  );
+};
+
+ReactDOM.render(<BookList/>, document.getElementById('root'));
